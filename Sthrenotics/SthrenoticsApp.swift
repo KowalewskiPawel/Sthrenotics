46429193cc5c4f55e132fa72f91078a9d90ff888
KowalewskiPawel <pawelkowalewsk@gmail.com>
2025-06-19 16:51:24 +0200

    Initial Commit

---
+++ Sthrenotics/SthrenoticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SthrenoticsApp.swift
+//  Sthrenotics
+//
+//  Created by Pawel Kowalewski on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SthrenoticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
